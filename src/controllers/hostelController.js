612146469fefb287185e1e301a6f123574334035
Sathya LK <rajavslk@gmail.com>
2020-05-13 00:42:24 +0530

    removed exclamations yuck

--- src/controllers/hostelController.js
+++ src/controllers/hostelController.js
@@ -4,34 +4,34 @@ exports.getAllhostels = (req, res) => {
   res.status(500).json({
     status: 'error',
     requestedAt: timestamp,
-    message: 'This route is not yet defined!',
+    message: 'This route is not yet defined',
   });
 };
 exports.gethostel = (req, res) => {
   res.status(500).json({
     status: 'error',
     requestedAt: timestamp,
-    message: 'This route is not yet defined!',
+    message: 'This route is not yet defined',
   });
 };
 exports.createhostel = (req, res) => {
   res.status(500).json({
     status: 'error',
     requestedAt: timestamp,
-    message: 'This route is not yet defined!',
+    message: 'This route is not yet defined',
   });
 };
 exports.updatehostel = (req, res) => {
   res.status(500).json({
     status: 'error',
     requestedAt: timestamp,
-    message: 'This route is not yet defined!',
+    message: 'This route is not yet defined',
   });
 };
 exports.deletehostel = (req, res) => {
   res.status(500).json({
     status: 'error',
     requestedAt: timestamp,
-    message: 'This route is not yet defined!',
+    message: 'This route is not yet defined',
   });
 };
