2cf02b9e2af8b72d2fbc093545169e704ee456b4
Sathya LK <rajavslk@gmail.com>
2020-05-13 00:23:12 +0530

    boilerplate hostel controller

---
+++ src/controllers/hostelController.js
@@ -0,0 +1,30 @@
+exports.getAllhostels = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+exports.gethostel = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+exports.createhostel = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+exports.updatehostel = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+exports.deletehostel = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
