f54dd4121662c9f43d53cc891c3936410b1cb848
Sathya LK <rajavslk@gmail.com>
2020-05-13 00:40:57 +0530

    added timestamp to hostelController

--- src/controllers/hostelController.js
+++ src/controllers/hostelController.js
@@ -1,30 +1,37 @@
+const timestamp = require('../middleware/timestamp');
+
 exports.getAllhostels = (req, res) => {
   res.status(500).json({
     status: 'error',
+    requestedAt: timestamp,
     message: 'This route is not yet defined!',
   });
 };
 exports.gethostel = (req, res) => {
   res.status(500).json({
     status: 'error',
+    requestedAt: timestamp,
     message: 'This route is not yet defined!',
   });
 };
 exports.createhostel = (req, res) => {
   res.status(500).json({
     status: 'error',
+    requestedAt: timestamp,
     message: 'This route is not yet defined!',
   });
 };
 exports.updatehostel = (req, res) => {
   res.status(500).json({
     status: 'error',
+    requestedAt: timestamp,
     message: 'This route is not yet defined!',
   });
 };
 exports.deletehostel = (req, res) => {
   res.status(500).json({
     status: 'error',
+    requestedAt: timestamp,
     message: 'This route is not yet defined!',
   });
 };
