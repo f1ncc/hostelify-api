106ce8d2259d65c3add3cc198319e6485d4e18ff
Sathya LK <rajavslk@gmail.com>
2020-05-12 22:55:14 +0530

    added middleware

---
+++ src/middleware/check_id.js
@@ -0,0 +1,10 @@
+// module.exports = (req, res, next, value) => {
+//   if (!(parseInt(req.params.id) in users.IDs)) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID',
+//     });
+//   }
+//   console.log(`ID: ${value}`);
+//   next();
+// };
